77ad071167e7f16d1de1eb8b3cd68fa6e8d03976
Xiao Gui <xgui3783@gmail.com>
2019-01-09 22:27:51 +0100

    init

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const request = require('request')
 
 app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'localhost')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
   next()
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+/**
+ * temporary solution
+ * solr instance does not come with cors header. need to proxy pass response
+ */
+const solrRoot = `http://imedv02.ime.kfa-juelich.de:8983/solr/inm_metadata_test/select`
+app.get('/proxyGetSolr', (req, res) => {
+  const {originalUrl} = req
+  const url = originalUrl.replace('/proxyGetSolr', solrRoot)
+  request(url).pipe(res)
+})
 
 const PORT = process.env.PORT || 3001
 
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs'
+/// <reference types = "javascript" />
 
 interface InteractiveViewer {
 
